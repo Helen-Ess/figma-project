404eb0aef3873ba0b92cb40aee06d072ee39720f
Helen-Ess <116563114+Helen-Ess@users.noreply.github.com>
2023-01-13 14:26:18 +0300

    Figma-project
    Figma-project

---
+++ components/SocialCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card shaped elevation="24">
+        <v-card-text>
+        <v-row align="center">
+           Join other industry-leading organizations pushing boundaries and solving problems in&nbsp;Figma
+           <v-spacer/>
+           <v-img width="64" src="https://cdn.sanity.io/images/599r6htc/localized/5cc4ec7efbf529d23d98eb5cea426716848fbd56-77x77.svg?q=75&fit=max&auto=format"/>
+           <v-img width="64" src="https://cdn.sanity.io/images/599r6htc/localized/ae44fb1f0119d84e2fdadaadf20c7c6f3cc57e76-78x77.svg?q=75&fit=max&auto=format"/>
+           <v-img width="64" src="https://cdn.sanity.io/images/599r6htc/localized/17b5b66d29a94a2e06bec865e350c55563ac4b59-78x77.svg?q=75&fit=max&auto=format"/>        
+        </v-row>
+        </v-card-text>
+    </v-card>
+</template>
---
+++ pages/1_pricing.vue
@@ -0,0 +1,24 @@
+<template>
+<v-container>
+  <v-col align="center">
+    <PricingSelect v-on:setType="setType"/>
+    <h1>Pricing</h1>
+  </v-col>
+  <div style="
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 32px;
+  margin: 80px auto 40px;
+  margin-top: 20px">
+      <PricingCard v-bind:data="data[type][0]"/>
+      <PricingCard v-bind:data="data[type][1]"/>
+      <PricingCard v-bind:data="data[type][2]"/>
+  </div>
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage'
+}
+</script>
